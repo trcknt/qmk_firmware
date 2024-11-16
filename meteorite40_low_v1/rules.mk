70182ccec850521dbfee60e32a1188a48ee519f3
iwakami <ryutaro.iwakami@autify.com>
2024-11-17 00:35:19 +0900

    Change directory structure

--- meteorite40_low_v1/rules.mk
+++ meteorite40_low_v1/rules.mk
@@ -12,10 +12,13 @@ AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = no
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = pmw3360
-ENCODER_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
+#ENCODER_ENABLE = yes
+#ENCODER_MAP_ENABLE = yes
 LTO_ENABLE = yes
 
 ifdef DEBUG
 	CONSOLE_ENABLE = yes         # Console for debug
 endif
+
+# Include common library
+SRC += lib/meteorite40.c
