80dabbb88fb21b7d0bdc5a4d0a321405bc81530a
iwakami <ryutaro.iwakami@autify.com>
2024-12-08 14:57:57 +0900

    Change keymap

--- meteorite40/meteorite40_high_v2/keymaps/develop/keymap.c
+++ meteorite40/meteorite40_high_v2/keymaps/develop/keymap.c
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                         KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                         KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , MS_BTN1   , MS_BTN2   , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
-    LT(4,KC_ESC), KC_NO, MT(MOD_LGUI,KC_TAB), MT(MOD_LSFT,KC_SPC),LT(1,KC_LNG1),LT(5,KC_LNG2), LT(2,KC_ENT), LT(3,KC_BSPC), KC_NO, KC_DEL
+    LT(4,KC_ESC), KC_NO, MT(MOD_LGUI,KC_TAB), MT(MOD_LSFT,KC_SPC),LT(1,KC_LNG1),LT(5,KC_LNG2), LT(2,KC_ENT), LT(3,KC_BSPC), KC_NO, QK_KB_9
   ),
 
   [1] = LAYOUT(
@@ -106,10 +106,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [4] = LAYOUT(
-    _______  , _______  , _______  , _______  , _______  ,                         _______  , _______  , _______  , _______  , _______ ,
-    _______  , _______  , _______  , _______  , _______  ,                         _______  , _______  , _______  , _______  , _______ ,
-    _______  , _______  , _______  , _______  , _______  , QK_KB_4   , QK_KB_5   , _______  , _______  , _______  , _______  , _______ ,
-    _______  ,            _______  , _______  , _______  , _______   , _______   , _______  , _______  , _______  ,              _______
+    QK_KB_10  , _______  , _______  , _______  , _______  ,                         QK_KB_4 , QK_KB_0  , QK_KB_1  , _______  , _______,
+    _______  , _______  , _______  , _______  , _______  ,                         _______  , QK_KB_5  , QK_KB_6  , _______  , _______ ,
+    _______  , _______  , _______  , _______  , _______  , QK_KB_2   , QK_KB_3   , _______  , QK_KB_7  , QK_KB_8  , _______  , _______ ,
+    _______  ,            _______  , _______  , _______  , _______   , _______   , _______  , _______  , _______  ,              QK_KB_11
   ),
 
   [5] = LAYOUT(
@@ -124,32 +124,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(MS_WHLD, MS_WHLU),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    [1] = { ENCODER_CCW_CW(UG_HUED, UG_HUEU),  ENCODER_CCW_CW(UG_SATD, UG_SATU)  },
-    [2] = { ENCODER_CCW_CW(UG_VALD, UG_VALU),  ENCODER_CCW_CW(UG_SPDD, UG_SPDU)  },
-    [3] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT),  ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
-    [4] = { ENCODER_CCW_CW(QK_KB_0, QK_KB_1),  ENCODER_CCW_CW(QK_KB_6, QK_KB_7)  },
+    [1] = { ENCODER_CCW_CW(MS_WHLD, MS_WHLU),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [2] = { ENCODER_CCW_CW(MS_WHLD, MS_WHLU),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [3] = { ENCODER_CCW_CW(MS_WHLD, MS_WHLU),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [4] = { ENCODER_CCW_CW(QK_KB_0, QK_KB_1),  ENCODER_CCW_CW(QK_KB_5, QK_KB_6)  },
     [5] = { ENCODER_CCW_CW(KC_NO, KC_NO),  ENCODER_CCW_CW(KC_NO, KC_NO) },
 };
 #endif
 
-
-/*
-enum meteorite_keycodes {
-    CPI_DEC    = QK_KB_0, //トラックボールのCPIを減少
-    CPI_INC    = QK_KB_1, //トラックボールのCPIを増加
-
-    ROT_DEC    = QK_KB_3, //マウスセンサーのＹ軸を反時計回りに10度回転
-    ROT_INC    = QK_KB_4, //マウスセンサーのＹ軸を時計回りに10度回転
-    SCAL_MOD   = QK_KB_5, //マウスセンサーのスケーリング有効・無効
-
-    SCR_DEC    = QK_KB_6, //スクロールの感度を減少
-    SCR_INC    = QK_KB_7, //スクロールの感度を増加
-
-	SCR_H_REV  = QK_KB_8, //水平のスクロール方向を反転
-    SCR_V_REV  = QK_KB_9, //垂直のスクロール方向を反転
-    SCR_MO     = QK_KB_10, //押している間スクロールモードが有効
-
-	KBC_RST    = QK_KB_11, //設定をリセット
-    KBC_SAV    = QK_KB_12, //設定を保存
-};
-*/
