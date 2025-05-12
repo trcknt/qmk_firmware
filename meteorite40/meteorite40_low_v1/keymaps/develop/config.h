3fe6e694f463c23c95181526ede8052e48873faa
trcknt <101651952+trcknt@users.noreply.github.com>
2025-05-13 01:12:58 +0900

    Update config.h

--- meteorite40/meteorite40_low_v1/keymaps/develop/config.h
+++ meteorite40/meteorite40_low_v1/keymaps/develop/config.h
@@ -19,5 +19,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
-#define TAPPING_TERM 200
+#define TAP_CODE_DELAY 5
+#define TAPPING_TERM 180
+
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#define KEYBALL_CPI_DEFAULT 700      // 光学センサーPMW3360DM の解像度 (CPI) の規定値
+#define KEYBALL_SCROLL_DIV_DEFAULT 3 // スクロール速度の規定値
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
